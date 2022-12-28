3a312479521aab6764b496cb26b3a0b4d838e332
Rafael Alberto Larios <albertolarios@me.com>
2022-12-28 12:48:29 -0600

    Download array of locations with geocoder api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const queryCityName = function(locStr) {
+    const apiKey = 'a6fb94b0479663119d883f5691dcff36';
+    locStr = locStr || 'Guadalajara';
+    fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${locStr}&appid=${apiKey}`,
+        { mode: 'cors'}
+    )
+    .then((response) => response.json())
+    .then((response) => console.log(response));
+}
+
+queryCityName();
