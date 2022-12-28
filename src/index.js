e356d5c5f7e533225fd2625026b8bc7d52c8c6c2
Rafael Alberto Larios <albertolarios@me.com>
2022-12-28 13:28:14 -0600

    Use coordinates from geocoder api to fetch weather data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import './style.css';
 
+const apiKey = 'a6fb94b0479663119d883f5691dcff36';
+const queryWeather = function(lat, lon, mode='current') {
+    if (['forecast', 'current'].includes(mode)) {
+        if (mode === 'current') mode = 'weather'
+    } else throw Error(`Invalid argument: ${mode}`)
+
+    fetch(
+        `https://api.openweathermap.org/data/2.5/${mode}?lat=${lat}&lon=${lon}&appid=${apiKey}`,
+        { mode: 'cors'}
+    )
+    .then((response) => response.json())
+    .then((response) => console.log(response));
+};
+
 const queryCityName = function(locStr) {
-    const apiKey = 'a6fb94b0479663119d883f5691dcff36';
     locStr = locStr || 'Guadalajara';
     fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${locStr}&appid=${apiKey}`,
         { mode: 'cors'}
     )
     .then((response) => response.json())
-    .then((response) => console.log(response));
+    .then((response) => {
+        const lat = response[0].lat;
+        const lon = response[0].lon;
+        console.log(lat, lon);
+        const coordinates = { lat: lat, lon: lon };
+        queryWeather(coordinates.lat, coordinates.lon);
+    });
 }
 
 queryCityName();
