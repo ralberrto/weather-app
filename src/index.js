f096b817f43e297d9eae47d4b6feb69547a1b7e6
Rafael Alberto Larios <albertolarios@me.com>
2022-12-28 14:45:52 -0600

    Catch data promis from reponse

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import './style.css';
 
 const apiKey = 'a6fb94b0479663119d883f5691dcff36';
-const queryWeather = function(lat, lon, mode='current') {
+const queryWeather = function(lat, lon, mode='forecast') {
     if (['forecast', 'current'].includes(mode)) {
         if (mode === 'current') mode = 'weather'
     } else throw Error(`Invalid argument: ${mode}`)
 
-    fetch(
+    const dataPr = fetch(
         `https://api.openweathermap.org/data/2.5/${mode}?lat=${lat}&lon=${lon}&appid=${apiKey}`,
         { mode: 'cors'}
     )
     .then((response) => response.json())
-    .then((response) => console.log(response));
+    .then((response) => response);
+    return dataPr;
 };
 
 const queryCityName = function(locStr) {
     locStr = locStr || 'Guadalajara';
-    fetch(
+    const dataPr = fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${locStr}&appid=${apiKey}`,
         { mode: 'cors'}
     )
     .then((response) => response.json())
-    .then((response) => {
-        const lat = response[0].lat;
-        const lon = response[0].lon;
-        console.log(lat, lon);
-        const coordinates = { lat: lat, lon: lon };
-        queryWeather(coordinates.lat, coordinates.lon);
+    .then((searchArray) => {
+        return queryWeather(searchArray[0].lat, searchArray[0].lon);
     });
+
+    return dataPr;
 }
 
-queryCityName();
+const data = queryCityName().then((data) => console.log(data));
