2364b376b66a2f9602929d8cb7bfb0a2d5377daf
Rafael Alberto Larios <albertolarios@me.com>
2022-12-28 09:48:33 -0600

    Set initial project's state

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather App
+
+Provides a UI to make requests to the [OpenWeather API](https://openweathermap.org/) based on the provided location.
+The site uses [webpack.js](https://webpack.js.org/) as [build system](https://en.wikipedia.org/wiki/Build_automation).
+And vanilla css and javascript for the frontend.
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    devtool: "inline-source-map",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woof|woof2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
