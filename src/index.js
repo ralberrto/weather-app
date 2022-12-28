6d994c5ac68920e053f15357f2f519031276a474
Rafael Alberto Larios <albertolarios@me.com>
2022-12-28 14:49:16 -0600

    Place weatherApi logic into a module

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 import './style.css';
 
-const apiKey = 'a6fb94b0479663119d883f5691dcff36';
-const queryWeather = function(lat, lon, mode='forecast') {
-    if (['forecast', 'current'].includes(mode)) {
-        if (mode === 'current') mode = 'weather'
-    } else throw Error(`Invalid argument: ${mode}`)
+const weatherApi = (function() {
+    const apiKey = 'a6fb94b0479663119d883f5691dcff36';
 
-    const dataPr = fetch(
-        `https://api.openweathermap.org/data/2.5/${mode}?lat=${lat}&lon=${lon}&appid=${apiKey}`,
-        { mode: 'cors'}
-    )
-    .then((response) => response.json())
-    .then((response) => response);
-    return dataPr;
-};
+    const queryWeather = function(lat, lon, mode='forecast') {
+        if (['forecast', 'current'].includes(mode)) {
+            if (mode === 'current') mode = 'weather'
+        } else throw Error(`Invalid argument: ${mode}`)
 
-const queryCityName = function(locStr) {
-    locStr = locStr || 'Guadalajara';
-    const dataPr = fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${locStr}&appid=${apiKey}`,
-        { mode: 'cors'}
-    )
-    .then((response) => response.json())
-    .then((searchArray) => {
-        return queryWeather(searchArray[0].lat, searchArray[0].lon);
-    });
+        const dataPr = fetch(
+            `https://api.openweathermap.org/data/2.5/${mode}?lat=${lat}&lon=${lon}&appid=${apiKey}`,
+            { mode: 'cors'}
+        )
+        .then((response) => response.json())
+        .then((response) => response);
+        return dataPr;
+    };
 
-    return dataPr;
-}
+    const queryCityName = function(locStr) {
+        locStr = locStr || 'Guadalajara';
+        const dataPr = fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${locStr}&appid=${apiKey}`,
+            { mode: 'cors'}
+        )
+        .then((response) => response.json())
+        .then((searchArray) => {
+            return queryWeather(searchArray[0].lat, searchArray[0].lon);
+        });
 
-const data = queryCityName().then((data) => console.log(data));
+        return dataPr;
+    }
+
+    return { queryCityName };
+
+})();
+
+const data = weatherApi.queryCityName().then((data) => console.log(data));
