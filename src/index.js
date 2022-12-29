9d7c15b7b022aac64d45f81ab9d20d990d3fac63
Rafael Alberto Larios <albertolarios@me.com>
2022-12-28 19:21:46 -0600

    Dynamically generate list of query matches if more than one

--- src/index.js
+++ src/index.js
@@ -7,17 +7,83 @@ icon.setAttribute('id', 'icon');
 const header = document.querySelector('header');
 header.prepend(icon);
 
+const mainCont = document.getElementById('main-container');
+
 const onSubmit = function(event) {
     event.preventDefault();
     const search = searchInput.value;
     searchInput.value = '';
-    weatherApi.queryCityName(search).then((data) => console.log(data));
+    weatherApi.queryCityName(search).then((entryList) => {
+        if (entryList.length > 1) {
+            const domList = DomGenerator.createEntryList(entryList);
+            mainCont.appendChild(domList);
+        } else if (entryList.length == 1) {
+            weatherApi.queryWeather(entryList[0].lat, entryList[0].lon, 'current')
+                .then((response) => console.log(response));
+        }
+    });
 };
 
 const searchForm = document.querySelector('form#search');
 searchForm.addEventListener('submit', onSubmit);
 const searchInput = document.getElementById('search-box');
 
+const DomGenerator = (function() {
+
+    const _appendChildren = function(target, children) {
+        const elements = Array.prototype.slice.call(arguments, 1);
+        for (let element of elements) {
+            target.appendChild(element);
+        }
+    };
+
+    const createEntryList = function(entryList) {
+        const list = document.createElement('ul');
+        entryList.forEach((entry) => {
+            const domEntry = createEntry(
+                entry.name,
+                entry.country,
+                entry.lat,
+                entry.lon
+            );
+            list.appendChild(domEntry);
+        });
+        return list;
+    };
+
+    const createEntry = function(city, country, lat, lon) {
+        const entryCont = document.createElement('li');
+        entryCont.className = 'search-entry';
+
+        const entryCity = document.createElement('p');
+        entryCity.className = 'entry-city';
+        entryCity.textContent = city;
+
+        const entryCountry = document.createElement('p');
+        entryCountry.className = 'entry-country';
+        entryCountry.textContent = country;
+
+        const entryCoord = document.createElement('ul');
+        entryCoord.className = 'entry-coordinates';
+
+        const latitude = document.createElement('li');
+        latitude.className = 'entry-latitude';
+        latitude.textContent = lat;
+
+        const longitude = document.createElement('li');
+        longitude .className = 'entry-longitude';
+        longitude .textContent = lon;
+
+        _appendChildren(entryCoord, latitude, longitude);
+        _appendChildren(entryCont, entryCity, entryCountry, entryCoord);
+
+        return entryCont;
+    };
+
+    return {
+        createEntryList,
+    }
+})();
 
 const weatherApi = (function() {
     const apiKey = 'a6fb94b0479663119d883f5691dcff36';
@@ -44,13 +110,13 @@ const weatherApi = (function() {
         )
         .then((response) => response.json())
         .then((searchArray) => {
-            return queryWeather(searchArray[0].lat, searchArray[0].lon);
+            return searchArray;
         });
 
         return dataPr;
     }
 
-    return { queryCityName };
+    return { queryCityName, queryWeather };
 
 })();
 
