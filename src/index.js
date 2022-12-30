868a6977dc38c323c4ebd7e02001f27e70ce627b
Rafael Alberto Larios <albertolarios@me.com>
2022-12-29 19:10:27 -0600

    Abstract away functionality into module, display default on load

--- src/index.js
+++ src/index.js
@@ -2,36 +2,48 @@ import './style.css';
 import iconSrc from './logo_white_cropped.png';
 import _ from 'lodash';
 
-const icon = new Image();
-icon.src = iconSrc;
-icon.setAttribute('id', 'icon');
-const header = document.querySelector('header');
-header.prepend(icon);
+const controller = (function() {
 
-const mainCont = document.getElementById('main-container');
+    const icon = new Image();
+    icon.src = iconSrc;
+    icon.setAttribute('id', 'icon');
+    const header = document.querySelector('header');
+    header.prepend(icon);
 
-const onSubmit = function(event) {
-    event.preventDefault();
-    const search = searchInput.value;
-    searchInput.value = '';
-    weatherApi.queryCityName(search).then((entryList) => {
-        if (entryList.length > 1) {
-            const domList = DomGenerator.createEntryList(entryList);
-            mainCont.appendChild(domList);
-        } else if (entryList.length == 1) {
-            weatherApi.queryWeather(entryList[0].lat, entryList[0].lon, 'current')
-                .then((response) => {
-                const currentWeather = DomGenerator.displayCurrentWeather(response);
-                console.log(response);
-                mainCont.appendChild(currentWeather);
-                });
-        }
-    });
-};
+    const mainCont = document.getElementById('main-container');
 
-const searchForm = document.querySelector('form#search');
-searchForm.addEventListener('submit', onSubmit);
-const searchInput = document.getElementById('search-box');
+    const displaySearch = function(search) {
+        weatherApi.queryCityName(search).then((entryList) => {
+            if (entryList.length > 1) {
+                const domList = DomGenerator.createEntryList(entryList);
+                mainCont.appendChild(domList);
+            } else if (entryList.length == 1) {
+                weatherApi.queryWeather(entryList[0].lat, entryList[0].lon, 'current')
+                    .then((response) => {
+                    const currentWeather = DomGenerator.displayCurrentWeather(response);
+                    console.log(response);
+                    mainCont.appendChild(currentWeather);
+                    });
+            }
+        });
+    }
+
+    const onSubmit = function(event) {
+        event.preventDefault();
+        mainCont.innerHTML = '';
+        const search = searchInput.value;
+        searchInput.value = '';
+        displaySearch(search);
+    };
+
+    const searchForm = document.querySelector('form#search');
+    searchForm.addEventListener('submit', onSubmit);
+    const searchInput = document.getElementById('search-box');
+
+    return {
+        displaySearch
+    };
+})();
 
 const DomGenerator = (function() {
 
@@ -181,4 +193,5 @@ const weatherApi = (function() {
 
 })();
 
+controller.displaySearch();
 //const data = weatherApi.queryCityName().then((data) => console.log(data));
